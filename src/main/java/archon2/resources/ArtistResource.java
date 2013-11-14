fe9c11ca8e7a026204ba8ce08a20512a8937e646
EJ Bensing <ebensing@rm-dash-r.com>
2013-11-14 00:15:41 -0500

    Added Jackson for JSON, cleaned up code a bit

--- src/main/java/archon2/resources/ArtistResource.java
+++ src/main/java/archon2/resources/ArtistResource.java
@@ -4,8 +4,8 @@ import archon2.actors.ActorController;
 import archon2.actors.ActorMessage;
 import archon2.actors.ArtistMessageTypes;
 import archon2.data.Artist;
+import org.codehaus.jackson.map.ObjectMapper;
 
-import javax.json.JsonObject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -22,14 +22,19 @@ import javax.ws.rs.core.MediaType;
 @Path("/artist")
 public class ArtistResource {
 
+    private ObjectMapper objectMapper = new ObjectMapper();
+
     @POST
     @Path("/add")
     @Consumes("application/json")
     @Produces(MediaType.APPLICATION_JSON)
-    public void addArtist(final JsonObject jo, @Suspended final AsyncResponse asyncResponse) {
-        // json parsing, this can be factored out for an actual Json lib
-        Artist art = new Artist(true);
-        art.setName(jo.getString("name"));
+    public void addArtist(final String jo, @Suspended final AsyncResponse asyncResponse) {
+        Artist art = null;
+        try {
+            art = objectMapper.readValue(jo, Artist.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         // create the message and send it to our actor
         ActorMessage msg = new ActorMessage(ArtistMessageTypes.CREATE, art, asyncResponse);
@@ -40,9 +45,13 @@ public class ArtistResource {
     @Path("/add/new")
     @Consumes("application/json")
     @Produces(MediaType.APPLICATION_JSON)
-    public void addNewArtist(final JsonObject req, @Suspended final AsyncResponse asyncResponse) {
-        Artist art = new Artist();
-        art.setName(req.getString("name"));
+    public void addNewArtist(final String req, @Suspended final AsyncResponse asyncResponse) {
+        Artist art = null;
+        try {
+            art = objectMapper.readValue(req, Artist.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         ActorMessage msg = new ActorMessage(ArtistMessageTypes.FORCECREATE, art, asyncResponse);
         ActorController.artistActor.tell(msg, ActorController.artistActor);
     }
